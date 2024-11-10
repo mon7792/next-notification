8eca6cdeefe106e407a6fc8ab918f76ee1ef6e49
mon7792 <monish7792@gmail.com>
2024-11-10 13:19:47 +0000

    fix: enabled debounce on done toggle

--- client/src/components/Activity/Item/ActivityItem.tsx
+++ client/src/components/Activity/Item/ActivityItem.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useCallback } from "react";
+import debounce from "lodash.debounce";
+import type { CheckedState } from "@radix-ui/react-checkbox";
+
 import type { Activity } from "@/types";
 import { cn } from "@/lib/utils";
 import { useUpdateActivity } from "@/api/mutation/activity";
@@ -7,9 +13,16 @@ import { Checkbox } from "@/components/ui/checkbox";
 const ActivityItem = ({ activity }: { activity: Activity }) => {
 	const { mutate: updateActivity } = useUpdateActivity();
 
-	const handleMarkAsDone = () => {
-		updateActivity({ id: activity.id, activity: { ...activity, done: !activity.done } });
+	const handleMarkAsDone = (e: CheckedState) => {
+		debouncedMarkTodoDone(activity.id, Boolean(e));
 	};
+
+	const debouncedMarkTodoDone = useCallback(
+		debounce((activityId: string, isDone: boolean) => {
+			updateActivity({ id: activityId, activity: { ...activity, done: isDone } });
+		}, 200),
+		[],
+	);
 
 	return (
 		<div className="flex flex-row items-center gap-4 w-[380px] rounded-lg border p-4">
